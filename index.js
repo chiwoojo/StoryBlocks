b56dd8736ec8a95ab6bf4cd8259aa8eff9ff3789
Vincent Jo <chiwoojo@gmail.com>
2016-01-04 14:57:34 -0800

    added username changing mechanism and messaging works now

--- index.js
+++ index.js
@@ -7,17 +7,27 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
+  
   console.log("A User Connected.");
+  
   //check for 'chat message' events
   socket.on('chat message', function(msg) {
-    //emit that message to all the users connect to the database
+    //emit that message to all the users connect to the database (to client)
     io.emit('chat message', msg);
     //log the message in node console
     console.log('message: ' + msg);
   });
+
+  socket.on('add user', function(username) {
+    // store the username in the socket session for this client
+    socket.username = username;
+    console.log('set username to ', socket.username);
+  });
+  
   socket.on('disconnect', function() {
     console.log('user disconnected..');
   });
+  
 });
 
 http.listen(3000, function() {
