4b807c00e080063aa2fc2a971bd17a865f15d1f3
Vincent Jo <chiwoojo@gmail.com>
2016-01-04 13:59:41 -0800

    finished example chat application from socket.io

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   console.log("A User Connected.");
+  //check for 'chat message' events
+  socket.on('chat message', function(msg) {
+    //emit that message to all the users connect to the database
+    io.emit('chat message', msg);
+    //log the message in node console
+    console.log('message: ' + msg);
+  });
   socket.on('disconnect', function() {
     console.log('user disconnected..');
   });
